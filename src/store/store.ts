d456dcb7ba895d0baa0b7e1dee0cc717061591ef
Richard Fu <richardfxr.personal@gmail.com>
2023-03-05 02:24:41 -0500

    🟠SVLT: add UGN stores

---
+++ src/store/store.ts
@@ -0,0 +1,22 @@
+/* === IMPORTS ============================ */
+import { browser } from '$app/environment';
+import { writable, get, type Writable } from 'svelte/store';
+import type { GlobeInstance } from 'globe.gl';
+
+/* === INTERFACES ========================= */
+export interface Country {
+    id: string,
+    totalPopulation: number
+};
+
+/* === STORES ============================= */
+export const UGNglobe: Writable<GlobeInstance | null> = writable(null);
+export const UGNcountryInfo: Writable<{ [key: string]: {name: string, altName?: string, flag: string, lat: number, lng: number} }> = writable({});
+export const UGNpopulationData: Writable<{ time: string, countries: { id: string, population: number[][] }[] }[]> = writable([]);
+export const UGNcurTimeIndex = writable(0);
+export const UGNcurCountries: Writable<Country[]> = writable([]);
+export const UGNglobeHoverCountry: Writable<Country | null> = writable(null);
+export const UGNlistHoverCountry: Writable<Country | null> = writable(null);
+export const UGNsexSelection = writable([0]);
+export const UGNageSelection = writable([0, 1, 2]);
+export const UGNverticalLayout = writable(false);
