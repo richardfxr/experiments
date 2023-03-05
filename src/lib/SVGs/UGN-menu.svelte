ad332629d3a7a07c2b42f5ab5bc05a7113276996
Richard Fu <richardfxr.personal@gmail.com>
2023-03-05 02:26:55 -0500

    🟠SVLT: add 4 UGN SVGs

---
+++ src/lib/SVGs/UGN-menu.svelte
@@ -0,0 +1,8 @@
+<svg
+    class="icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 15 13">
+    <path fill="currentcolor" d="m14,2H1c-.55,0-1-.45-1-1S.45,0,1,0h13c.55,0,1,.45,1,1s-.45,1-1,1Z"/>
+    <path fill="currentcolor" d="m14,13H1c-.55,0-1-.45-1-1s.45-1,1-1h13c.55,0,1,.45,1,1s-.45,1-1,1Z"/>
+    <path fill="currentcolor" d="m14,7.5H1c-.55,0-1-.45-1-1s.45-1,1-1h13c.55,0,1,.45,1,1s-.45,1-1,1Z"/>
+</svg>
