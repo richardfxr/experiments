75486218539fd1f449a12c537821d5c4d3d7b4be
Richard Fu <richardfxr.personal@gmail.com>
2023-03-10 23:59:16 -0500

    📐DES: add plus and minus icons

---
+++ src/lib/SVGs/UGN-minus.svelte
@@ -0,0 +1,7 @@
+<svg
+    id="Layer_2"
+    class="icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 15 15">
+    <path fill="currentcolor" d="m14,8.5H1c-.55,0-1-.45-1-1s.45-1,1-1h13c.55,0,1,.45,1,1s-.45,1-1,1Z"/>
+</svg>
---
+++ src/lib/SVGs/UGN-plus.svelte
@@ -0,0 +1,7 @@
+<svg
+    id="plus"
+    class="icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 15 15">
+    <path fill="currentcolor" d="m14,6.5h-5.5V1c0-.55-.45-1-1-1s-1,.45-1,1v5.5H1c-.55,0-1,.45-1,1s.45,1,1,1h5.5v5.5c0,.55.45,1,1,1s1-.45,1-1v-5.5h5.5c.55,0,1-.45,1-1s-.45-1-1-1Z"/>
+</svg>
