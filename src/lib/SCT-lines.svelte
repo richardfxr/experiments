f0a54fb0a966b41c27e318781ed4dbddec0e44e1
Richard Fu <richardfxr.personal@gmail.com>
2023-09-10 16:13:02 -0400

    🟠SVLT: create Scatter lines component

---
+++ src/lib/SCT-lines.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    /* === PROPS ============================== */
+    export let numPoints: number;
+    export let origin: { x: number, y: number};
+    export let points: { x: number, y: number }[];
+    export let animationCompleted: boolean;
+</script>
+
+
+
+<svg
+    id="lines"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 100 100"
+    preserveAspectRatio="none">
+    {#each {length: points.length} as _, i (i)}
+        <line
+            class:shown={animationCompleted && i < numPoints}
+            x1={origin.x}
+            y1={origin.y}
+            x2={points[i].x}
+            y2={points[i].y}
+            vector-effect="non-scaling-stroke" />
+    {/each}
+</svg>
+
+
+
+<style lang="scss">
+    #lines {
+        width: 100%;
+        height: 100%;
+
+        line {
+            stroke: var(--clr-line);
+            opacity: 0;
+
+            transition: opacity 0s linear;
+
+            &.shown {
+                opacity: 1;
+                transition: opacity var(--trans-normal) var(--trans-cubic-1);
+            }
+        }
+    }
+</style>
