1d1b15400ad2a886c927e158fbad84582e1844c0
Richard Fu <richardfxr.personal@gmail.com>
2023-09-20 22:00:25 -0400

    🟠SVLT: created /shader-maker & SHD-canvas component

---
+++ src/routes/shader-maker/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    /* === IMPORTS ============================ */
+    import Canvas from "$lib/SHD-canvas.svelte";
+</script>
+
+
+
+<main>
+    <Canvas />
+    <div class="controls"></div>
+</main>
+
+
+
+
+<style lang="scss" global>
+    main {
+        display: flex;
+        flex-flow: row nowrap;
+        background-color: black;
+    }
+
+    #canvas {
+        height: 100vh;
+        width: 100vh;
+        background-color: rebeccapurple;
+    }
+
+    // === MEDIA QUERIES ==========================
+    @media (orientation: portrait) {
+        main {
+            display: block;
+        }
+
+        #canvas {
+            height: 100vw;
+            width: 100vw;
+        }
+    }
+</style>
