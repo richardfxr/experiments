ffd8fc305eeaba2716bae416f69a304f6822d7e0
Richard Fu <richardfxr.personal@gmail.com>
2023-10-04 18:32:36 -0400

    🟠SVLT: add pallete function

--- src/shaders/test.glsl
+++ src/shaders/test.glsl
@@ -3,6 +3,15 @@
 uniform vec3 iResolution;
 uniform float iTime;
 
+vec3 palette( in float t ) {
+    vec3 a = vec3(0.500, 0.500, 0.500);
+    vec3 b = vec3(0.500, 0.500, 0.500);
+    vec3 c = vec3(1.000, 1.000, 1.000);
+    vec3 d = vec3(0.498, 0.268, 1.068);
+
+    return a + b*cos( 6.28318*(c*t+d) );
+}
+
 void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
     // ranges from -1 to 1 for both x and y. Centered at (0, 0)
     vec2 uv = fragCoord / iResolution.xy * 2.0 - 1.0;
@@ -14,12 +23,16 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
     }
 
     float dis = length(uv);
-    dis = sin(dis * 8. - 2. * iTime) / 8.;
+    vec3 color = palette(dis);
+
+    dis = sin(dis * 35. - 5.5 * iTime) / 10.;
     dis = abs(dis);
 
-    dis = smoothstep(0.0, 0.1, dis);
+    dis = 0.05 / dis;
 
-    fragColor = vec4(0.0, dis, dis, 1.0);
+    color *= dis;
+
+    fragColor = vec4(color, 1.0);
 }
 
 void main() {
