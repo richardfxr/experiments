24d111c902a63f1bb490b5ff0ca38dd2c63e4e6b
Richard Fu <richardfxr.personal@gmail.com>
2024-02-25 16:40:15 -0500

    🟠SVLT: create Image component

---
+++ src/lib/image.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    /* === PROPS ============================== */
+    export let src: string;
+    export let alt: string;
+</script>
+
+
+
+<picture>
+    <source srcset="{src}.avif" type="image/avif" />
+    <source srcset="{src}.webp" type="image/webp" />
+    <img src="{src}.jpg" {alt} />
+</picture>
