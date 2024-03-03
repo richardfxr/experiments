b72be002060a6496229e11d549f93da338f844af
Richard Fu <richardfxr.personal@gmail.com>
2024-03-03 17:53:19 -0500

    🟠SVLT: create PXL-lockInput component

---
+++ src/lib/PXL-lockInput.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    /* === IMPORTS ============================ */
+    import Lock from "$lib/SVGs/PXL-lock.svelte";
+
+    /* === PROPS ============================== */
+    export let id: string;
+    export let locked: boolean; // bind
+</script>
+
+
+
+<div class="lockInput">
+    <input
+        type="checkbox"
+        {id}
+        class="visuallyHidden"
+        bind:checked={locked} />
+    <label for={id} class="iconButton">
+        <Lock {locked} />
+        <span class="visuallyHidden">
+            <slot />
+        </span>
+    </label>
+</div>
+
+
+
+<style lang="scss">
+    .lockInput {
+        input {
+            &:focus-visible + .iconButton,
+            &:active + .iconButton {
+                color: var(--PXL-clr-bg);
+                background-color: var(--PXL-clr-900);
+            }
+        }
+
+        .iconButton {
+            display: block;
+            cursor: pointer;
+        }
+    }
+</style>
