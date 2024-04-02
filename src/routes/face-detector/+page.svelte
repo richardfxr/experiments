00bf04e13d534b2aa7aac9aae376bdd3f222018d
Richard Fu <richardfxr.personal@gmail.com>
2024-04-01 22:43:44 -0400

    🟠SVLT: redraw all rectangles every frame for better fade effect

--- src/routes/face-detector/+page.svelte
+++ src/routes/face-detector/+page.svelte
@@ -29,7 +29,15 @@
         width: number;
         height: number;
         rotation: number;
+        drawnAt: DOMHighResTimeStamp;
     };
+
+    /* === CONSTANTS ========================== */
+    const timeToFade = 4000;
+    const backgroundHue = 30;
+    const backgroundSaturation = 100;
+    const backgroundLightness = 44;
+    const lightnessFadeSpeed = (100 - backgroundLightness) / timeToFade;
 
     /* === VARIABLES ========================== */
     let state: State = "loading";
@@ -44,7 +52,8 @@
         width: null,
         height: null
     }
-    let detections: { detections: Detection[] };
+    let rectangles: Rectangle[] = [];
+    let confidence: number[] = [];
 
     /* === FUNCTIONS ========================== */
     function handleResize(): void {
@@ -62,10 +71,20 @@
         canvasRatio.height = canvas.height / videoSize.height;
     }
 
-    function drawRectangle(rectangle: Rectangle): void {
-        if (!canvasRatio.width ||  !canvasRatio.height) return;
-
+    function drawRectangle(
+        rectangle: Rectangle,
+        timestamp: DOMHighResTimeStamp
+    ): void {
+        if (!canvasRatio.width || !canvasRatio.height) return;
+        const elapsedTime = timestamp - rectangle.drawnAt;
         context.save();
+
+        context.fillStyle = `hsl(
+            ${backgroundHue},
+            ${backgroundSaturation}%,
+            ${Math.max(100 - (elapsedTime * lightnessFadeSpeed), backgroundLightness)}%
+        )`;
+
         if (
             rectangle.rotation !== 0 &&
             rectangle.rotation !== Math.PI
@@ -84,7 +103,6 @@
         }
 
         // draw rectangle
-        context.fillStyle = "#ffffff";
         context.fillRect(
             rectangle.originX * canvasRatio.width,
             rectangle.originY * canvasRatio.height,
@@ -93,6 +111,80 @@
         );
 
         context.restore();
+    }
+
+    function renderNewFrame(
+        detections: Detection[],
+        timestamp: DOMHighResTimeStamp
+    ): void {
+        if (
+            canvasRatio.width === null ||
+            canvasRatio.height === null
+        ) return;
+
+        // clear canvas
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        // draw background
+        context.fillStyle = `hsl(
+            ${backgroundHue},
+            ${backgroundSaturation}%,
+            ${backgroundLightness}%
+        )`;
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        let firstValidRectangle = 0;
+        // draw all existing rectangles
+        for (let i = 0; i < rectangles.length; i++) {
+            const rectangle = rectangles[i];
+            drawRectangle(rectangle, timestamp);
+
+            if (rectangle.drawnAt + timeToFade <= timestamp) {
+                // rectangle has faded out
+                firstValidRectangle = i + 1;
+            }
+        }
+
+        // remove all rectangles that have faded out
+        if (firstValidRectangle > 0)
+            rectangles = rectangles.slice(firstValidRectangle);
+
+        if (videoSize.width === null) return;
+        confidence = [];
+
+        // go through new detections from this frame
+        for(let i = 0; i < detections.length; i++) {
+            const detection = detections[i];
+            if (!detection.boundingBox) return;
+
+            confidence.push(Math.round(detection.categories[0].score * 100));
+
+            // get both eyes
+            const leftEye = detection.keypoints[0];
+            const rightEye = detection.keypoints[1];
+            if (!leftEye || !rightEye) return;
+
+            // set up values for drawing
+            const flippedRotation = Math.PI - Math.atan2(
+                rightEye.y - leftEye.y,
+                rightEye.x - leftEye.x
+            );
+            const flippedX = videoSize.width - detection.boundingBox.originX - detection.boundingBox.width;
+
+            // construct new rectangle
+            const newRectangle: Rectangle = {
+                originX: flippedX,
+                originY: detection.boundingBox.originY,
+                width: detection.boundingBox.width,
+                height: detection.boundingBox.height,
+                rotation: flippedRotation,
+                drawnAt: timestamp
+            };
+
+            // add new rectangle to array and draw it
+            rectangles.push(newRectangle);
+            drawRectangle(newRectangle, timestamp);
+        }
     }
 
     async function enableWebcam(): Promise<void> {
@@ -112,14 +204,14 @@
                     height: video.offsetHeight
                 };
                 handleResize();
-                predictWebcam();
+                window.requestAnimationFrame(predictWebcam);
             });
         } catch (err) {
             console.error(err);
         }
     }
 
-    async function predictWebcam(): Promise<void> {
+    async function predictWebcam(timestamp: DOMHighResTimeStamp): Promise<void> {
         if (!video) return;
         if (video.currentTime === videoPrevTime) {
             // frame has alreay been drawn
@@ -129,40 +221,9 @@
 
         const startTimestamp = performance.now();
         videoPrevTime = video.currentTime;
-        detections = faceDetector.detectForVideo(video, startTimestamp);
+        const res = faceDetector.detectForVideo(video, startTimestamp);
 
-        context.fillStyle = 'rgba(0, 0, 0, 0.01)';
-        context.fillRect(0, 0, canvas.width, canvas.height);
-
-        detections.detections.forEach(detection => {
-            if (
-                !detection.boundingBox ||
-                videoSize.width === null ||
-                videoSize.height === null ||
-                canvasRatio.width === null ||
-                canvasRatio.height === null
-            ) return;
-
-            // get both eyes
-            const leftEye = detection.keypoints[0];
-            const rightEye = detection.keypoints[1];
-            if (!leftEye || !rightEye) return;
-
-            // set up values for drawing
-            const flippedRotation = Math.PI - Math.atan2(
-                rightEye.y - leftEye.y,
-                rightEye.x - leftEye.x
-            );
-            const flippedX = videoSize.width - detection.boundingBox.originX - detection.boundingBox.width;
-
-            drawRectangle({
-                originX: flippedX,
-                originY: detection.boundingBox.originY,
-                width: detection.boundingBox.width,
-                height: detection.boundingBox.height,
-                rotation: flippedRotation
-            });
-        });
+        renderNewFrame(res.detections, timestamp);
 
         window.requestAnimationFrame(predictWebcam);
     }
@@ -172,12 +233,20 @@
         handleResize();
 
         // get 2D canvas context
-        const tempContext = canvas.getContext("2d");
+        const tempContext = canvas.getContext("2d", { alpha: false });
         if (!tempContext) {
             state = "noContext";
             return;
         }
         context = tempContext;
+
+        // draw background
+        context.fillStyle = `hsl(
+            ${backgroundHue},
+            ${backgroundSaturation}%,
+            ${backgroundLightness}%
+        )`;
+        context.fillRect(0, 0, canvas.width, canvas.height);
 
         try {
             // load Mediapipe WASM
@@ -232,39 +301,6 @@
 
     <div class="webcamView">
         <video bind:this={video} autoplay></video>
-        {#if state === "active"}
-            {#each detections.detections as detection}
-                <p
-                    class="text"
-                    style="
-                        left: {detection.boundingBox ? detection.boundingBox.originX : 0}px;
-                        top: calc({detection.boundingBox ? detection.boundingBox.originY : 0}px - 1rem - 5px);
-                    ">
-                    Confidence: {Math.round(parseFloat(detection.categories[0].score.toString()) * 100)}%
-                </p>
-                <div
-                    class="boundingBox"
-                    style="
-                        left: {detection.boundingBox ? detection.boundingBox.originX : 0}px;
-                        top: {detection.boundingBox ? detection.boundingBox.originY : 0}px;
-                        width: {detection.boundingBox ? detection.boundingBox.width : 0}px;
-                        height: {detection.boundingBox ? detection.boundingBox.height : 0}px;
-                    ">
-                </div>
-
-                {#each detection.keypoints as keypoint}
-                    <div
-                        class="boundingBox"
-                        style="
-                            left: {keypoint.x * video.offsetWidth - 4}px;
-                            top: {keypoint.y * video.offsetHeight - 4}px;
-                            width: 8px;
-                            height: 8px;
-                        ">
-                    </div>
-                {/each}
-            {/each}
-        {/if}
     </div>
 </main>
 
@@ -276,7 +312,7 @@
         width: 100%;
         height: 100vh;
 
-        background-color: #000000;
+        background-color: #e07100;
     }
 
     .webcamView {
@@ -284,6 +320,13 @@
         overflow: hidden;
 
         font-family: sans-serif;
+
+        video {
+            position: fixed;
+            top: 0;
+            left: 0;
+            z-index: -1;
+        }
 
         .text {
             position: absolute;
